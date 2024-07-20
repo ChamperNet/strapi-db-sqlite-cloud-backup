11bc17ecba80f97e4fc865aac3fd537c68005d15
Timur Iskakov <info@champer.ru>
2024-07-21 00:53:14 +0400

    ESLint reformat and version bumped

--- cli.js
+++ cli.js
@@ -1,35 +1,35 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
-import fs from 'fs';
-import path from 'path';
-import { config } from 'dotenv';
-import { backupDatabase } from './backup.js';
+import { Command } from 'commander'
+import fs from 'fs'
+import path from 'path'
+import { config } from 'dotenv'
+import { backupDatabase } from './backup.js'
 
-const program = new Command();
-program.version('1.0.0');
+const program = new Command()
+program.version('1.0.0')
 
 program
   .command('init')
   .description('Initialize the backup configuration')
   .action(() => {
-    const templatePath = path.resolve(__dirname, 'template.env');
-    const destinationPath = path.resolve(process.cwd(), '.env');
+    const templatePath = path.resolve(__dirname, 'template.env')
+    const destinationPath = path.resolve(process.cwd(), '.env')
 
     if (!fs.existsSync(destinationPath)) {
-      fs.copyFileSync(templatePath, destinationPath);
-      console.log('.env file has been created. Please fill in the required values.');
+      fs.copyFileSync(templatePath, destinationPath)
+      console.log('.env file has been created. Please fill in the required values.')
     } else {
-      console.log('.env file already exists.');
+      console.log('.env file already exists.')
     }
-  });
+  })
 
 program
   .command('run')
   .description('Run the backup script')
   .action(() => {
-    config(); // Load .env variables
-    backupDatabase(); // Run backup
-  });
+    config() // Load .env variables
+    backupDatabase() // Run backup
+  })
 
-program.parse(process.argv);
+program.parse(process.argv)
